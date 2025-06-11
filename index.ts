d6bb81ce53449a818f8ff1ce7dd65061ed702c84
kaseywright <kasey.wright@biblionexus.org>
2025-06-10 20:39:49 -0400

    Update basic index.ts example output

--- index.ts
+++ index.ts
@@ -1,12 +1,20 @@
 import { sort, Classification } from "./sort";
 import type { Package } from "./sort";
 
-const pkg: Package = {
+let pkg: Package = {
     width: 100,
     height: 50,
     length: 30,
     mass: 10
 };
 
-const result: Classification = sort(pkg);
-console.log(`Package classification: ${result}`);
+let result: Classification = sort(pkg);
+console.log(`Package classification: ${result}  - ${pkg.width} x ${pkg.height} x ${pkg.length} x ${pkg.mass}`);
+
+pkg.width = 200;
+result = sort(pkg);
+console.log(`Package classification: ${result}  - ${pkg.width} x ${pkg.height} x ${pkg.length} x ${pkg.mass}`);
+
+pkg.mass = 25;
+result = sort(pkg);
+console.log(`Package classification: ${result}  - ${pkg.width} x ${pkg.height} x ${pkg.length} x ${pkg.mass}`);
